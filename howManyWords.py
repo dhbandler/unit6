37c7da15aa24894db7869d76d53446317058fbd1
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-10 08:30:07 -0400

    Updated from Brython Server: 5/10/2018 8:30:07 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,11 @@
+#Daniel Bandler
+#5/10/18
+#howManyWords.py prints out how many words per number of letter
 
+file = open("engmix.txt")
+
+length = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for line in file:
+    length[len(line)-1] += 1
+print(length)
