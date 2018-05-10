71084402bd0d919ec5f37ce3b3ae95533119afe9
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-10 08:10:13 -0400

    Updated from Brython Server: 5/10/2018 8:10:13 AM

--- lastWordDemo.py
+++ lastWordDemo.py
@@ -1,1 +1,12 @@
+#Daniel Bandler
+#5/10/18
+#lastWordDemo.py print last word of each line of a file
 
+file = open("fileDemo.py")
+
+for line in file:
+    words = line.split()
+    if len(words) >0:
+        print(words[-1])
+    else:
+        print()
