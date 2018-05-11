2367729acd48c1c4e580eca285d176bed6ae38ec
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-11 08:27:35 -0400

    Updated from Brython Server: 5/11/2018 8:27:34 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,12 @@
+#Daniel Bandler
+#5/11/18
+#fileDemo.py finding file
 
+file = open("engmix.txt")
+
+for line in file:
+    line = line.strip()
+    if len(line) > 0 and line[0] == "d" and line[-1] == "b":
+        print(line)
+
+    
