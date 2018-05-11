d64bf6284cb3e1c35fba5548017f54fde0063d9f
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-11 09:49:28 -0400

    Update reverseFile.py

--- reverseFile.py
+++ reverseFile.py
@@ -2,7 +2,7 @@
 #5/10/18
 #reverseFile.py prints out palindrome
 
-fileForOpen =input("enter a file name here")
+fileForOpen = input("enter a file name here")
 file = open(fileForOpen) 
 
 lines = []
@@ -10,3 +10,14 @@ for line in file:
     lines.append(line.split())
 
 print(lines.reverse())
+
+
+"""
+ask for file
+open file
+make empty list
+for loop that goes through each line
+    add each line to list
+reverse list
+print out each line in list
+"""
