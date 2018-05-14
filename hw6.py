723fa1b22e93478bdc810b8150b8e39e3c77b6a3
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-14 08:16:04 -0400

    Updated from Brython Server: 5/14/2018 8:16:04 AM

--- hw6.py
+++ hw6.py
@@ -1,1 +1,26 @@
+#Daniel Bandler
+#5/14/18
+#hw6.py
 
+
+"""
+#Program 1
+wordlist = ["dog", "cat", "hamburguesa"]
+wordguess = input("Guess a word")
+if wordguess in wordlist:
+    print("yes")
+else:
+    print("no")
+"""
+"""
+file = open("engmix.txt")
+
+numLines = 0
+for line in file:
+    numLines += 1
+    if numLines == 888:
+        print(line.strip())
+"""
+file = open("palindromes.txt")
+for line in file:
+    print(line.strip(), "!")
