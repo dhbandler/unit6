096d7cf8a5796397fbd89bbfc6f4c7e08975e394
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-16 08:30:55 -0400

    Updated from Brython Server: 5/16/2018 8:30:54 AM

--- RNA.py
+++ RNA.py
@@ -2,5 +2,9 @@
 #5/16/18
 #E_Coli.py gives you explosive bowel problems
 
-genome = open("E_Coli
+genome = open("E_coli_genome.txt")
+
+for i in genome:
+    genome.replace("T","U")
+print(genome)
 
