06ebad1894d0aed96f85fb5f9e673130ae738d00
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-17 08:26:23 -0400

    Updated from Brython Server: 5/17/2018 8:26:22 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,10 @@
+#Daniel Bandler
+#5/17/18
+#warmup17.py - checks for letters of name in dictionart
 
+file = open("engmix.txt")
+
+for line in file:
+    line = line.strip()
+    if "b" in line and "a" in line and "n" in line and "d" in line and "l" in line and "e" in line and "r" in line:
+        print(line)
