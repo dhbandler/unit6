7cbd1b7e64cc473789ca7ee5da244da09c4be315
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-17 08:38:25 -0400

    Updated from Brython Server: 5/17/2018 8:38:24 AM

--- palindromes.py
+++ palindromes.py
@@ -3,10 +3,13 @@
 #palindromes.py
 
 
-file = open("palindromes.py")
+file = open("engmix.txt")
 
 
 for line in file:
-    line = line.split()
-    if line == line.reverse():
+    line = line.strip()
+    back = ""
+    for ch in line:
+        back = ch + back
+    if back == line:
         print(line)
