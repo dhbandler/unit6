a41481539ebcf892724a34023c9ebc72483eb4d2
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-17 08:49:08 -0400

    Updated from Brython Server: 5/17/2018 8:49:07 AM

--- anagram.py
+++ anagram.py
@@ -1,1 +1,23 @@
+#Daniel Bandler
+#5/17/18
+#anagram.py prints out all anagrams of word in dictionary
 
+file = open("engmix.txt")
+
+word1 = input("what is the word you want to anagramize? ")
+
+word1List = []
+word2List = []
+
+for char in word1:
+    word1List.append(char)
+    word1List.sort()
+
+for line in file:
+    line = line.strip()
+    for let in line:
+        word2List.append(let)
+        word2List.sort()
+    if word2List == word1List:
+        print(line)
+    word2List = []
