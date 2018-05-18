814742e98a85efbe2eac7026c9b937ef9d75a9df
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-18 08:49:00 -0400

    Updated from Brython Server: 5/18/2018 8:48:59 AM

--- testreview.py
+++ testreview.py
@@ -1,1 +1,29 @@
+#Daniel Bandler
+#5/18/18
+#testreview.py
+
+"""
+#1 find all word in dictionary that have 3 letter "c"s and 2 letter "p"s
+
+file = open("engmix.txt")
+
+
+for line in file:
+
+    if line.count("p") == 2 and line.count("c") == 3:
+        print(line)
+
+"""
+#2 print out words that start with letter r    
+
+file = open("engmix.txt")
+wordList = []
+wordCount = 0
+for line in file:
+    for let in line:
+        wordList.append(let)
+    if wordList[0] == "r":
+        wordCount += 1  
+    wordList = []
+    print(wordCount)
 
