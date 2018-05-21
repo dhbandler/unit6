6eee68f78b13363bfd6ac05ac27695722ad5ec61
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-21 08:06:28 -0400

    Updated from Brython Server: 5/21/2018 8:06:27 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,19 @@
+#Daniel Bandler
+#5/21/18
+#quiz6.py --- tests brains
 
+#Program #1:
+file = open("engmix.txt")
+
+let = input("Type a letter")
+
+letInLine = 0
+
+for line in file:
+    line = line.strip()
+    for ch in line:
+        if ch == let:
+            letInLine += 1
+        if letInLine == 4:
+            print(line)
+    letInLine = 0
